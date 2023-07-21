bd098a399566fb27786ce66c43e9b52bbc9e8e8f
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-21 15:35:08 +0800

    fix: rnft login manager prefab

---
+++ Runtime/Auth/RNFTLogin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RNFTLogin : MonoBehaviour
+{
+    [SerializeField] private Button mainButton;
+    [SerializeField] private GameObject loginContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainButton.onClick.RemoveAllListeners();
+        mainButton.onClick.AddListener(LoginUser);
+    }
+
+    private void LoginUser()
+    {
+        Debug.Log("onLoginClicked ");
+        RNFTAuthManager _authenticationManager = new RNFTAuthManager();
+        string loginUrl = _authenticationManager.GetLoginUrl();
+        Application.OpenURL(loginUrl);
+    }
+
+}
