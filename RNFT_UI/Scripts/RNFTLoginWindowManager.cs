209da7281c572ffdb0b53b23d21c1d6282b0a0c0
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-25 16:20:25 +0800

    feat: function to verify the otp of the user

--- RNFT_UI/Scripts/RNFTLoginWindowManager.cs
+++ RNFT_UI/Scripts/RNFTLoginWindowManager.cs
@@ -8,15 +8,37 @@ using Amazon.CognitoIdentityProvider;
 
 public class RNFTLoginWindowManager : MonoBehaviour
 {
+
+    public static RNFTLoginWindowManager Instance { get; private set; }
+
     [SerializeField] private InputField email;
     [SerializeField] private InputField otp;
     [SerializeField] private Button loginButton;
     [SerializeField] private Button generateOTPButton;
 
+    // submitted email and the session class vaiables
+    public string submittedEmail;
+    public string session; 
+
     void Start()
     {
         SetupLoginButton();
         SetupGenerateOTPButton();
+    }
+
+    void Awake()
+    {
+        Debug.Log("[RNFT] Passwordless Auth Manager Awake!");
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     // function to setup listeners for the generate otp button
@@ -49,10 +71,10 @@ public class RNFTLoginWindowManager : MonoBehaviour
     // function to handle the login button click
     private void HandleLoginButtonClick()
     {
-        // both otp and email are required to login
-        if (otp.text != "" && email.text != "")
+        // submitted email should not be "" or null
+        if (otp.text != "" && submittedEmail != "" && submittedEmail != null)
         {
-            Debug.Log("otp is " + otp.text + " and email is " + email.text);
+            VerifyUserOTP(submittedEmail, otp.text);
         }
         else
         {
@@ -62,7 +84,7 @@ public class RNFTLoginWindowManager : MonoBehaviour
 
 
     // helper functions
-    public static void SignInUser(string email)
+    public void SignInUser(string email)
     {
         // create a new instance of the cognito identity provider client
         AmazonCognitoIdentityProviderClient providerClient = new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials(), RegionEndpoint.APSoutheast1);
@@ -89,8 +111,47 @@ public class RNFTLoginWindowManager : MonoBehaviour
         // initiate the auth request and store the response
         Amazon.CognitoIdentityProvider.Model.InitiateAuthResponse authResponse = providerClient.InitiateAuthAsync(authRequest).Result;
 
+        // store the session and the email that was submitted in class variables
+        session = authResponse.Session;
+        submittedEmail = email;
+
         // log the response challenge type 
         Debug.Log("[RNFT] " + authResponse.ChallengeName + " has been initiated.");
+    }
+
+
+    // function to verify the otp entered by the user
+    public void VerifyUserOTP(string email, string otp)
+    {
+        // create a response to the auth challenge
+        Amazon.CognitoIdentityProvider.Model.RespondToAuthChallengeRequest authChallengeResponse = new Amazon.CognitoIdentityProvider.Model.RespondToAuthChallengeRequest();
+
+        // set the client id
+        authChallengeResponse.ClientId = RNFTAuthConfig.UserPoolClientID;
+
+        // set the challenge name
+        authChallengeResponse.ChallengeName = Amazon.CognitoIdentityProvider.ChallengeNameType.CUSTOM_CHALLENGE;
+
+        // set the session
+        authChallengeResponse.Session = session;
+
+        // define the challenge responses
+        Dictionary<string, string> challengeResponses = new Dictionary<string, string>();
+        challengeResponses.Add("USERNAME", email);
+        challengeResponses.Add("ANSWER", otp);
+
+        // set the challenge responses
+        authChallengeResponse.ChallengeResponses = challengeResponses;
+
+        // create a new instance of the cognito identity provider client
+        AmazonCognitoIdentityProviderClient providerClient = new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials(), RegionEndpoint.APSoutheast1);
+
+        // respond to the auth challenge and store the response
+        Amazon.CognitoIdentityProvider.Model.RespondToAuthChallengeResponse authChallengeResponseResult = providerClient.RespondToAuthChallengeAsync(authChallengeResponse).Result;
+
+        // log the response challenge type
+        Debug.Log("[RNFT] The custom challenge has been responded to.");        
+
     }
 }
 
