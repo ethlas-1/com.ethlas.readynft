e788c57b953f3124bc6d742db0500708ebae9a06
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-25 11:28:55 +0800

    feat: new ui prefab

---
+++ Runtime/Auth/RNFTUIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RNFTUIManager : MonoBehaviour
+{
+    [SerializeField] private Button loginWithRNFTButton;
+    [SerializeField] private GameObject uiContainer;
+    [SerializeField] private GameObject loginWindow;
+    [SerializeField] private GameObject loggedInWindow;
+
+    void Start()
+    {
+        SetupRNFTButtonListeners();
+    }
+
+    // function to setup listeners for the ready nft button
+    private void SetupRNFTButtonListeners()
+    {
+        loginWithRNFTButton.onClick.RemoveAllListeners();
+        loginWithRNFTButton.onClick.AddListener(ShowLoginWindow);
+    }
+
+    // function to show the login window once it is clicked
+    private void ShowLoginWindow()
+    {
+        uiContainer.SetActive(true);
+        loginWindow.SetActive(true);
+    }
+    
+}
