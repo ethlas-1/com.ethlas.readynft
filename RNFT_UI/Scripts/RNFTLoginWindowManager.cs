6230db0a389abcd06a3e3a11944ad1854d9d5787
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-25 15:50:59 +0800

    feat: initiate a login challenge to authenticate the user

--- RNFT_UI/Scripts/RNFTLoginWindowManager.cs
+++ RNFT_UI/Scripts/RNFTLoginWindowManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Amazon;
+using Amazon.CognitoIdentityProvider;
 
 public class RNFTLoginWindowManager : MonoBehaviour
 {
@@ -37,6 +39,7 @@ public class RNFTLoginWindowManager : MonoBehaviour
         if (email.text != "")
         {
             Debug.Log("email is " + email.text);
+            SignIn(email.text);
         }
         else
         {
@@ -57,6 +60,41 @@ public class RNFTLoginWindowManager : MonoBehaviour
             Debug.Log("OTP field is empty");
         }
     }
+
+    public void SignIn(string email)
+    {
+        // create a new instance of the cognito identity provider client
+        AmazonCognitoIdentityProviderClient providerClient = new AmazonCognitoIdentityProviderClient(new Amazon.Runtime.AnonymousAWSCredentials(), RegionEndpoint.APSoutheast1);
+
+        // create a new initiate auth request
+        Amazon.CognitoIdentityProvider.Model.InitiateAuthRequest authRequest = new Amazon.CognitoIdentityProvider.Model.InitiateAuthRequest();
+
+        // set the auth flow type to custom auth
+        authRequest.AuthFlow = Amazon.CognitoIdentityProvider.AuthFlowType.CUSTOM_AUTH;
+
+        // set the client id
+        Debug.Log("the client id is " + RNFTAuthConfig.UserPoolClientID);
+        authRequest.ClientId = RNFTAuthConfig.UserPoolClientID;
+
+        // define the auth parameters
+        Dictionary<string, string> authParams = new Dictionary<string, string>();
+        authParams.Add("USERNAME", email);
+        authParams.Add("PASSWORD", "password");
+        // password is not used for verfication. hence, it doesn't matter
+        // we use an otp to verify the user instead, which is sent to their email
+
+        // set the auth parameters
+        authRequest.AuthParameters = authParams;
+
+        // initiate the auth request and store the response
+        Amazon.CognitoIdentityProvider.Model.InitiateAuthResponse authResponse = providerClient.InitiateAuthAsync(authRequest).Result;
+
+        // log the response challenge type 
+        Debug.Log("response is " + authResponse.ChallengeName);
+
+
+    }
+
 
 
 }
