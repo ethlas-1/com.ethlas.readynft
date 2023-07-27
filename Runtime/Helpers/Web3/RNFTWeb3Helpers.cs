f4457a6e6a03fd7734dd4536bcdc8ba2dc8ed86b
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-27 08:20:23 +0800

    feat: mint nft with callback function

--- Runtime/Helpers/Web3/RNFTWeb3Helpers.cs
+++ Runtime/Helpers/Web3/RNFTWeb3Helpers.cs
@@ -58,5 +58,55 @@ public static class RNFTWeb3Helpers
 
         return false;
     }
+
+    // a variation of the gasless mint function that takes a call back function and executes it after the mint is successful
+    public static async Task<bool> MintNFTWithCallback(string walletAddress, string nftId, Action<bool> callback)
+    {
+
+        if (string.IsNullOrEmpty(walletAddress))
+        {
+            Debug.Log("MintNFT: Custodial wallet address not set!");
+            return false;
+        }
+
+        if (string.IsNullOrEmpty(nftId))
+        {
+            Debug.Log("MintNFT: NFT ID not set!");
+            return false;
+        }
+
+        string url = RNFTRequestsConfig.API_ENDPOINTS_ROOT_URL + RNFTRequestsConfig.API_MINT_NFT_ROUTE;
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                RNFTGaslessMintRequest requestData = new RNFTGaslessMintRequest(walletAddress, nftId);
+                string requestDataJson = JsonConvert.SerializeObject(requestData);
+                HttpContent content = new StringContent(requestDataJson, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    RNFTGaslessMintResponse result = JsonConvert.DeserializeObject<RNFTGaslessMintResponse>(responseString);
+                    callback(true);
+                    return true;
+                }
+                else
+                {
+                    Debug.Log("MintNFT: Request failed: " + response.StatusCode);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log("MintNFT: Request failed: " + e.Message);
+            }
+        }
+
+        callback(false);
+        return false;
+    }
 }
 
