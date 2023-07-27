cd0405245f30db546b7f9f4fe6917f3a6aa8be59
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-07-27 12:01:45 +0800

    feat: added some config files

---
+++ Runtime/Config/RNFTAuthConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class RNFTAuthConfig
+{
+	// user pool client id 
+	public const string UserPoolClientID = "7mibnn0t0cs4jj8c7r0pv85mgf";
+
+	// user pool id
+	public const string UserPoolID = "ap-southeast-1_PBKrUOzFG";
+}
+
