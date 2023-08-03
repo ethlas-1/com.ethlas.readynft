f752dd87fa1ef09fe96a5f9087ad3536aae61eca
fufuninja <17244262+fufuninja@users.noreply.github.com>
2023-08-03 21:32:48 +0800

    chore: resize

--- Runtime/Helpers/UIManagers/OTPInputManager.cs
+++ Runtime/Helpers/UIManagers/OTPInputManager.cs
@@ -10,11 +10,6 @@ public class OTPInputManager : MonoBehaviour
     public System.Action<bool> OnUserLoginCallback;
 
     [SerializeField] private InputField otp1;
-    [SerializeField] private InputField otp2;
-    [SerializeField] private InputField otp3;
-    [SerializeField] private InputField otp4;
-    [SerializeField] private InputField otp5;
-    [SerializeField] private InputField otp6;
     [SerializeField] private InputField emailField;
     [SerializeField] private Text helpText;
     [SerializeField] private Button backButton;
@@ -25,12 +20,7 @@ public class OTPInputManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        otp1.onValueChanged.AddListener(onValueChangeHanlder1);
-        otp2.onValueChanged.AddListener(onValueChangeHanlder2);
-        otp3.onValueChanged.AddListener(onValueChangeHanlder3);
-        otp4.onValueChanged.AddListener(onValueChangeHanlder4);
-        otp5.onValueChanged.AddListener(onValueChangeHanlder5);
-        otp6.onValueChanged.AddListener(onValueChangeHanlder6);
+        otp1.onValueChanged.AddListener(onValueChangeHanlder);
 
         backButton.onClick.RemoveAllListeners();
         backButton.onClick.AddListener(() => HandleBackButtonPress());
@@ -60,126 +50,21 @@ public class OTPInputManager : MonoBehaviour
     {
         otp1.Select();
         otp1.text = "";
-        otp2.text = "";
-        otp3.text = "";
-        otp4.text = "";
-        otp5.text = "";
-        otp6.text = "";
-
-        otp1.interactable = true;
-        otp2.interactable = false;
-        otp3.interactable = false;
-        otp4.interactable = false;
-        otp5.interactable = false;
-        otp6.interactable = false;
     }
 
-    private void onValueChangeHanlder1 (string input)
+    private void onValueChangeHanlder (string input)
     {
         if(input == "")
         {
             return;
         }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
+      
+        if(input.Length == 6)
         {
-            otp1.interactable = false;
-            otp2.interactable = true;
-            otp2.Select();
-        }
-    }
-
-    private void onValueChangeHanlder2 (string input)
-    {
-        if(input == "")
-        {
-            otp2.interactable = false;
-            otp1.interactable = true;
-            otp1.Select();
-            return;
-        }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
-        {
-            otp2.interactable = false;
-            otp3.interactable = true;
-            otp3.Select();
-        }
-    }
-
-    private void onValueChangeHanlder3 (string input)
-    {
-        if(input == "")
-        {
-            otp3.interactable = false;
-            otp2.interactable = true;
-            otp2.Select();
-            return;
-        }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
-        {
-            otp3.interactable = false;
-            otp4.interactable = true;
-            otp4.Select();
-        }
-    }
-
-    private void onValueChangeHanlder4 (string input)
-    {
-        if(input == "")
-        {
-            otp4.interactable = false;
-            otp3.interactable = true;
-            otp3.Select();
-            return;
-        }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
-        {
-            otp4.interactable = false;
-            otp5.interactable = true;
-            otp5.Select();
-        }
-    }
-
-    private void onValueChangeHanlder5 (string input)
-    {
-        if(input == "")
-        {
-            otp5.interactable = false;
-            otp4.interactable = true;
-            otp4.Select();
-            return;
-        }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
-        {
-            otp5.interactable = false;
-            otp6.interactable = true;
-            otp6.Select();
-        }
-    }
-
-    private void onValueChangeHanlder6 (string input)
-    {
-        if(input == "")
-        {
-            otp6.interactable = false;
-            otp5.interactable = true;
-            otp5.Select();
-            return;
-        }
-        int value = Int32.Parse(input);
-        if(value >= 0 && value <= 9)
-        {
-            //  otp6.interactable = false;
-            //  todo    - auto submit 6 OTP code!
-            string finalOTP = otp1.text + otp2.text + otp3.text + otp4.text + otp5.text + otp6.text;
-
             //  auto login once 6 digits are provided
-            HandleLoginButtonClick(RNFTLoginWindowManager.Instance.submittedEmail, finalOTP, RNFTLoginWindowManager.Instance.session);
+            HandleLoginButtonClick(RNFTLoginWindowManager.Instance.submittedEmail, input, RNFTLoginWindowManager.Instance.session);
         }
+
     }
 
     // function to handle the login button click
