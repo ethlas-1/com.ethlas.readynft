4ef038683bf1bf83cf203bc51d0424b7e180ed70
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-08-22 16:44:15 +0800

    chore: types for ghost wallet login and trf gw to rnft user

---
+++ Runtime/Types/Requests/GhostWallets/RNFTGhostWalletLoginTypes.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine.Scripting;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[Preserve]
+[Serializable]
+public class RNFTGhostWalletLoginRequest
+{
+	// euid
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+	// game_id
+	[Preserve]
+	[JsonProperty("game_id")]
+	public string game_id { get; set; } = "";
+
+	// default constructor
+	public RNFTGhostWalletLoginRequest()
+	{
+		// Default constructor
+	}	
+
+	// constructor
+	public RNFTGhostWalletLoginRequest(string _euid, string _game_id)
+	{
+		euid = _euid;
+		game_id = _game_id;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTGhostWalletLoginResponseData
+{
+	// euid
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+	// loggedIn
+	[Preserve]
+	[JsonProperty("loggedIn")]
+	public bool loggedIn { get; set; } = false;
+
+	// default constructor
+	public RNFTGhostWalletLoginResponseData()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTGhostWalletLoginResponseData(string _euid, bool _loggedIn)
+	{
+		euid = _euid;
+		loggedIn = _loggedIn;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTGhostWalletLoginResponse
+{
+	// data
+	[Preserve]
+	[JsonProperty("data")]
+	public RNFTGhostWalletLoginResponseData data { get; set; } = new RNFTGhostWalletLoginResponseData();
+
+	// default constructor
+	public RNFTGhostWalletLoginResponse()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTGhostWalletLoginResponse(RNFTGhostWalletLoginResponseData _data)
+	{
+		data = _data;
+	}
+}
+
---
+++ Runtime/Types/Requests/GhostWallets/RNFTTrfGWToRNFTUserTypes.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Scripting;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[Preserve]
+[Serializable]
+public class RNFTTrfGWToRNFTUserRequest
+{
+	// euid 
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+	// uuid
+	[Preserve]
+	[JsonProperty("uuid")]
+	public string uuid { get; set; } = "";
+
+	// default constructor
+	public RNFTTrfGWToRNFTUserRequest()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTTrfGWToRNFTUserRequest(string _euid, string _uuid)
+	{
+		euid = _euid;
+		uuid = _uuid;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTTrfGWToRNFTUserResponseData
+{
+	// transferred
+	[Preserve]
+	[JsonProperty("transferred")]
+	public bool transferred { get; set; } = false;
+
+	// default constructor
+	public RNFTTrfGWToRNFTUserResponseData()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTTrfGWToRNFTUserResponseData(bool _transferred)
+	{
+		transferred = _transferred;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTTrfGWToRNFTUserResponse
+{
+	// data
+	[Preserve]
+	[JsonProperty("data")]
+	public RNFTTrfGWToRNFTUserResponseData data { get; set; } = new RNFTTrfGWToRNFTUserResponseData();
+
+	// default constructor
+	public RNFTTrfGWToRNFTUserResponse()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTTrfGWToRNFTUserResponse(RNFTTrfGWToRNFTUserResponseData _data)
+	{
+		data = _data;
+	}
+}
+
