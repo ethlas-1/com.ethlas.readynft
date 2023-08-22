566602ef5165f19b50283fa9bd00dde1738ca6eb
KiranMo27 <112458705+KiranMo27@users.noreply.github.com>
2023-08-22 16:31:25 +0800

    chore: types for does gw exist and fetch gw

---
+++ Runtime/Types/Requests/GhostWallets/RNFTDoesGhostWalletExistTypes.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine.Scripting;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[Preserve]
+[Serializable]
+public class RNFTDoesGhostWalletExistRequest
+{
+	// euid
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+	// game_id
+	[Preserve]
+	[JsonProperty("game_id")]
+	public string game_id { get; set; } = "";
+
+	// default constructor
+	public RNFTDoesGhostWalletExistRequest()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTDoesGhostWalletExistRequest(string _euid, string _game_id)
+	{
+		euid = _euid;
+		game_id = _game_id;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTDoesGhostWalletExistResponseData
+{
+	// exists 
+	[Preserve]
+	[JsonProperty("exists")]
+	public bool exists { get; set; } = false;
+
+	// default constructor
+	public RNFTDoesGhostWalletExistResponseData()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTDoesGhostWalletExistResponseData(bool _exists)
+	{
+		exists = _exists;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTDoesGhostWalletExistResponse
+{
+	// data
+	[Preserve]
+	[JsonProperty("data")]
+	public RNFTDoesGhostWalletExistResponseData data { get; set; } = new RNFTDoesGhostWalletExistResponseData();
+
+	// default constructor
+	public RNFTDoesGhostWalletExistResponse()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTDoesGhostWalletExistResponse(RNFTDoesGhostWalletExistResponseData _data)
+	{
+		data = _data;
+	}
+}
+	
+
+
---
+++ Runtime/Types/Requests/GhostWallets/RNFTFetchGhostWalletTypes.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Scripting;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[Preserve]
+[Serializable]
+public class RNFTFetchGhostWalletRequest
+{
+	// euid
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+	// game_id
+	[Preserve]
+	[JsonProperty("game_id")]
+	public string game_id { get; set; } = "";
+
+	// default constructor
+	public RNFTFetchGhostWalletRequest()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTFetchGhostWalletRequest(string _euid, string _game_id)
+	{
+		euid = _euid;
+		game_id = _game_id;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTFetchGhostWalletResponseData
+{
+	// euid 
+	[Preserve]
+	[JsonProperty("euid")]
+	public string euid { get; set; } = "";
+
+
+	// wallet address
+	[Preserve]
+	[JsonProperty("wallet_address")]
+	public string wallet_address { get; set; } = "";
+
+	// default constructor
+	public RNFTFetchGhostWalletResponseData()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTFetchGhostWalletResponseData(string _euid, string _wallet_address)
+	{
+		euid = _euid;
+		wallet_address = _wallet_address;
+	}
+}
+
+[Preserve]
+[Serializable]
+public class RNFTFetchGhostWalletResponse
+{
+	// data
+	[Preserve]
+	[JsonProperty("data")]
+	public RNFTFetchGhostWalletResponseData data { get; set; } = new RNFTFetchGhostWalletResponseData();
+
+	// default constructor
+	public RNFTFetchGhostWalletResponse()
+	{
+		// Default constructor
+	}
+
+	// constructor
+	public RNFTFetchGhostWalletResponse(RNFTFetchGhostWalletResponseData _data)
+	{
+		data = _data;
+	}
+}
+
+
+
